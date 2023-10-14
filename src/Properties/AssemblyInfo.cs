a99f56494cf8e593c21742797724d1a85fe7e33e
rookiestyle <rookiestyle@gmx.net>
2023-10-14 11:15:23 +0200

    Add compatibility to KeePass 2.55
    Ensure plugin versions are shown in plugin overview

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ Various other enhancements, check the options")]
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("2.1.1")]
-[assembly: AssemblyFileVersion("2.1.1")]
+[assembly: AssemblyVersion("2.1.2")]
+[assembly: AssemblyFileVersion("2.1.2")]
 [assembly: Guid("78C152F3-EAF8-4FFC-9BE3-F5DC0CD66E5D")]
 
